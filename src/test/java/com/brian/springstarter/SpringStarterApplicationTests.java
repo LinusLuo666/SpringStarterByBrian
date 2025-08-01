81531d515deb517d62bf38b9967d6b6b40c71c43
Brian.Luo-罗旭 <Brian.Luo@fanruan.com>
2025-08-01 20:30:47 +0800

    Initial commit叮咚唧唧

---
+++ src/test/java/com/brian/springstarter/SpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.springstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
